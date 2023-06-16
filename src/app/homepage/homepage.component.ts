a28045a5914214b364c671c0e04bb4ce535cc1ff
Stef-w98 <stef.wouters18@gmail.com>
2023-06-16 20:07:40 +0200

    Added other languages to typewriter

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -14,16 +14,45 @@ export class HomepageComponent implements AfterViewInit {
   }
 
   startTypingAnimation(): void {
-    const text = "Hi, I'm Stef Wouters";
-    let index = 0;
+    const texts = [
+      "Hello, I'm Stef Wouters",
+      "Hallo, ik ben Stef Wouters",
+      "こんにちは、私はStef Woutersです",
+      "¡Hola, soy Stef Wouters!",
+      "Hallo, ich bin Stef Wouters",
+      "你好，我是Stef Wouters",
+      "Bonjour, je suis Stef Wouters"
+    ];
+    let textIndex = 0;
+    let charIndex = 0;
+    let isDeleting = false;
+    let delay = 100; // Delay between typing/deleting each character
 
     const typeNextCharacter = () => {
-      if (index < text.length) {
-        this.typingText += text.charAt(index);
-        index++;
-        this.typewriterRef.nativeElement.innerText = this.typingText;
-        setTimeout(typeNextCharacter, 100);
+      const currentText = texts[textIndex];
+
+      if (isDeleting) {
+        this.typingText = currentText.substring(0, charIndex);
+        charIndex--;
+      } else {
+        this.typingText = currentText.substring(0, charIndex + 1);
+        charIndex++;
       }
+
+      this.typewriterRef.nativeElement.innerText = this.typingText;
+
+      if (isDeleting && charIndex === -1) {
+        isDeleting = false;
+        textIndex = (textIndex + 1) % texts.length;
+        delay = 800; // Delay before typing the next text
+      } else if (!isDeleting && charIndex === currentText.length) {
+        isDeleting = true;
+        delay = 800; // Delay before deleting the current text
+      } else {
+        delay = 100; // Delay between typing/deleting each character
+      }
+
+      setTimeout(typeNextCharacter, delay);
     };
 
     typeNextCharacter();
