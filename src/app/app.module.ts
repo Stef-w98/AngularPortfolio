d7735f21d715354bb9322a8a7c7df9a0c42582ce
Stef-w98 <stef.wouters18@gmail.com>
2023-07-26 21:47:02 +0200

    Fixed form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -14,6 +12,9 @@ import { ProjectsComponent } from './projects/projects.component';
 import { PopupComponent } from './popup/popup.component';
 import { MedremindComponent } from './medremind/medremind.component';
 import { ContactComponent } from './contact/contact.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -31,8 +32,13 @@ import { ContactComponent } from './contact/contact.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatDialogModule,
-    FormsModule, // Add FormsModule here
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  contactForm!: FormGroup; // Add '!' to make it nullable
 
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.contactForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: [''],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      // Perform actions with the form values
+      console.log(this.contactForm.value);
+      // You can send the form data to your backend or handle it as required
+    } else {
+      // The form is invalid, show error messages or handle accordingly
+    }
+  }
 }
